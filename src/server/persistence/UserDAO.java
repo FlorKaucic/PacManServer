671c6c090777ac48d0263d6aa374baae9b72ad86
Flor Kaucic <florkaucic@gmail.com>
2015-11-06 17:17:40 -0300

    UserDao

--- src/server/persistence/UserDAO.java
+++ src/server/persistence/UserDAO.java
@@ -8,16 +8,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import game.logic.User;
+import game.logic.builder.UserBuilder;
 import server.config.Config;
 
 public class UserDAO {
 
 	public static User save(String username, String password) throws SQLException {
-		User user = new UserBuilder(id, username).build();
+		User user = UserBuilder(username).build();
 				
 		String url = "jdbc:mysql://" + Config.get("dbhost") + ":" + Config.get("dbport")+ "/" + Config.get("dbname");
 		Connection con = DriverManager.getConnection(url, Config.get("dbuser"), Config.get("dbpass"));
-		PreparedStatement st = con.prepareStatement("INSERT INTO tbl_user (username, password, nickname, won, lost) VALUES (?,?,?,?,?);");
+		PreparedStatement st = con.prepareStatement("INSERT INTO tbl_user (username, password, nickname, won, lost) "
+				+ "VALUES (?,?,?,?,?);");
 				
 		st.setString(1, user.getUsername());
 		st.setString(2, password);
@@ -27,10 +29,30 @@ public class UserDAO {
 		
 		boolean result = st.execute();
 		
-		st.close();
+		st.close();	
+		
+		if(!result){
+			con.close();
+			return null;
+		}
+			
+		st = con.prepareStatement("SELECT id FROM tbl_user WHERE username = ?;");
+		st.setString(1, user.getUsername());
+		
+		ResultSet rs = st.executeQuery();		
+		
+		if(!rs.first()){
+			con.close();
+			return null;
+		}
+		
+		user.setId(rs.getInt("id"));
+		
+		rs.close();
+		st.close();	
 		con.close();
 		
-		return (result)?user:null;
+		return user;
 	}
 
 	public static User get(String username, String password) throws SQLException {
