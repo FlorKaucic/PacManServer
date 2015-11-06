7e91e30c00c39b3bfa6c1bb2f764620f48d069bb
Flor Kaucic <florkaucic@gmail.com>
2015-11-06 16:14:59 -0300

    Otra mod en user

--- src/server/persistence/UserDAO.java
+++ src/server/persistence/UserDAO.java
@@ -12,16 +12,53 @@ import server.config.Config;
 
 public class UserDAO {
 
-	public static void save(User user) throws SQLException {
+	public static void save(User user, String password) throws SQLException {
 		String url = "jdbc:mysql://" + Config.get("dbhost") + ":" + Config.get("dbport")+ "/" + Config.get("dbname");
 		Connection con = DriverManager.getConnection(url, Config.get("dbuser"), Config.get("dbpass"));
 		PreparedStatement st = con.prepareStatement("INSERT INTO tbl_user (username, password, nickname, won, lost) VALUES (?,?,?,?,?);");
 		
 		st.setString(1, user.getUsername());
-		st.setString(2, user.getPassword());
+		st.setString(2, password);
 		st.setString(3, user.getUsername());
-		st.setString(4, user.getUsername());
-		st.setString(5, user.getUsername());
+		st.setInt(4, user.getWonMatches());
+		st.setInt(5, user.getLostMatches());
+		
+		st.execute();
+		
+		st.close();
+		con.close();
 	}
 
+	public static User get(String username, String password) throws SQLException {
+		String url = "jdbc:mysql://" + Config.get("dbhost") + ":" + Config.get("dbport")+ "/" + Config.get("dbname");
+		Connection con = DriverManager.getConnection(url, Config.get("dbuser"), Config.get("dbpass"));
+		PreparedStatement st = con.prepareStatement("SELECT * FROM tbl_user WHERE username = ? and password = ?;");
+		
+		st.setString(1, username);
+		st.setString(2, password);
+		
+		st.execute();
+		
+		st.close();
+		con.close();
+		
+		return new User();
+	}
+	
+	public static void update(User user) throws SQLException {
+		String url = "jdbc:mysql://" + Config.get("dbhost") + ":" + Config.get("dbport")+ "/" + Config.get("dbname");
+		Connection con = DriverManager.getConnection(url, Config.get("dbuser"), Config.get("dbpass"));
+		PreparedStatement st = con.prepareStatement("INSERT INTO tbl_user (username, password, nickname, won, lost) VALUES (?,?,?,?,?);");
+		
+		st.setString(1, user.getUsername());
+		st.setString(3, user.getUsername());
+		st.setInt(4, user.getWonMatches());
+		st.setInt(5, user.getLostMatches());
+		
+		st.execute();
+		
+		st.close();
+		con.close();
+	}
+	
 }
