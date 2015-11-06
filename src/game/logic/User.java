e4048fef385fca15dbb713e5f06a409092bc265b
Flor Kaucic <florkaucic@gmail.com>
2015-11-06 16:30:54 -0300

    User falta solo constructor

--- src/game/logic/User.java
+++ src/game/logic/User.java
@@ -10,20 +10,13 @@ public class User {
 	private int won;
 	private int lost;
 
+	
 	public int getId() {
 		return id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
-
 	public String getUsername() {
 		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
 	}
 
 	public String getNickname() {
